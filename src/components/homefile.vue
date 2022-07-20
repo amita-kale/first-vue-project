ace77b36cf8043fc9923372356c38d4d12c071b4
amita-kale <amitakale297@gmail.com>
2022-07-20 13:56:50 +0530

    done functionality of hide show using if directive

--- src/components/homefile.vue
+++ src/components/homefile.vue
@@ -1,10 +1,15 @@
 <template>
  
  <div>
-  <h1 v-if="show">show true then else text printed "Hello"</h1>
-  <h1 v-else>show false then else text printed "Hello"</h1>
+  <!-- <h1 v-if="show">show true then else text printed "Hello"</h1>
+  <h1 v-else>show false then else text printed "Hello"</h1> -->
   <h2>{{demo}}</h2>
-  <button v-on:click="hello('submit click')">Submit</button>
+  
+  <!-- <button v-on:click="hello('submit click')">Submit</button>  -->
+  <br>
+  <button v-on:click="show=!show">Show hide div</button>
+
+  <div class="div" v-if="show">"I am amita"</div>
   
  </div>
 </template>
@@ -12,16 +17,20 @@
 <script>
 export default {
   name:"home-file",
+  data(){
+    return {show:true}
+  },
   props: {
     //msg: String,
-    demo:String
+    demo:String,
+    
   },
   methods:{
-    hello(){
-      // console.log("hi submit clicked");
-      return{show:false}
+    // hello(){
+    //   // console.log("hi submit clicked");
+    //   return{show:true}
 
-    }
+    // }
     //parameterised fun
     //  hello(data){
     //   alert(data);
@@ -29,3 +38,13 @@ export default {
   }
 }
 </script>
+<style>
+.div{
+  background-color:aqua;
+  height: 20%;
+  width: 20%;
+  color: red;
+  align-items: center;
+ 
+}
+</style>
