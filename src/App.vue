a3817b1d31755b3a12bb823ff82eef9d7795327b
amita-kale <amitakale297@gmail.com>
2022-07-20 10:56:55 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<homefile/>
+</div>
+</template>
+
+<script>
+// import HelloWorld from './components/Hello-World.vue'
+import homefile from './components/homefile.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    homefile
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
