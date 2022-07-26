3d7c73c6d617b1008d2f0bb69b1324d0e5e649e0
amita-kale <amitakale297@gmail.com>
2022-07-26 17:06:48 +0530

    Form input binding done

--- src/components/demo.vue
+++ src/components/demo.vue
@@ -2,8 +2,12 @@
 <div>
     <h1>{{Title}}</h1>
     <h1>{{count}}</h1>
+    
     <br>
-   Name: <input type="text" placeholder="Enter Name" ref="name">  <br>
+   Name: <input type="text" placeholder="Enter Name" v-model="msg"> 
+   <h3>Msg is:{{msg}}</h3> <br>
+   Pick Any Color: <input type="color">  <br>
+
     <br>
     <button @click="buttonClicked()">click Here</button>
 </div>
@@ -12,8 +16,9 @@
 export default {
   name:"listFile",
   data(){
-    return {Title:"Welcome to my page",name:"Amita",count:0}
+    return {Title:"Welcome to my page",name:"Amita",count:0,msg:''}
   },
+ 
   methods:{
     buttonClicked(){
       this.count++;
