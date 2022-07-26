bef498ad480a7cfd187993a44b89b748dc9ec3c0
amita-kale <amitakale297@gmail.com>
2022-07-26 16:15:45 +0530

    text interpollation used

---
+++ src/components/demo.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <h1>{{name}}</h1>
+    <br>
+    <button>click Here</button>
+</div>
+</template>
+<script>
+export default {
+  name:"listFile",
+  data(){
+    return {name:"Welcome to my page"}
+  },
+  methods:{
+    buttonClicked(){
+    //   this.name="Rutika"
+    }
+  }
+}
+</script>
