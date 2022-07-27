7e11f13722b1a1b1c5d76313c183bbd24cf8a71c
amita-kale <amitakale297@gmail.com>
2022-07-27 18:19:37 +0530

    added table structure

--- src/components/signUpForm.vue
+++ src/components/signUpForm.vue
@@ -1,27 +1,55 @@
 <template>
- 
 <div>
-  <h1 style="color:blue">signUp Form</h1>
+<div >
+  <h1 style="color:red" class="font-bold text-3xl p-6">signUp Form</h1>
  
- ID:<input type="number" v-model="signUp.id"><br><br>
- Name:<input type="text" v-model="signUp.name"><br><br>
- Hobbies: &nbsp;&nbsp;<input type="checkbox" id="hob" value="kho-kho" v-model="signUp.hobbies">
- <label for="hob">Kho-Kho</label> &nbsp;
- <input type="checkbox" id="hobi" v-model="signUp.hobbies">
- <label for="hobi">Cricket</label><br>
-<br>
- Gender: &nbsp;&nbsp;<input type="radio" id="female" value="female" name="gender" v-model="signUp.gender">
- <label for="female">Female</label>&nbsp;
- <input type="radio" id="male" value="male" name="gender" v-model="signUp.gender">
- <label for="male">Male</label><br>
- <br>
- <button type="submit" v-on:click="submitFormValues()">Submit</button>
+        ID:<input type="number" class="bg-white  border border-slate-300 rounded-md mx-4 py-2 pl-9 pr-3" v-model="signUp.id" ><br><br>
+      
+        Name:<input type="text" class="bg-white  border border-slate-300 rounded-md py-2 pl-9 pr-3" v-model="signUp.name"><br><br>
+        Hobbies: &nbsp;&nbsp;<input type="checkbox" id="hob" value="kho-kho" v-model="signUp.hobbies">
+        <label for="hob">Kho-Kho</label> &nbsp;
+        <input type="checkbox" id="hobi" value="Cricket" v-model="signUp.hobbies">
+        <label for="hobi">Cricket</label><br>
+        <br>
+        Gender: &nbsp;&nbsp;<input type="radio" id="female" value="female" name="gender" v-model="signUp.gender">
+        <label for="female">Female</label>&nbsp;
+        <input type="radio" id="male" value="male" name="gender" v-model="signUp.gender">
+        <label for="male">Male</label><br>
+        <br>
+        <button type="submit" class="border-solid rounded border-2 border-indigo-600 p-3 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500" v-on:click="submitFormValues()">Submit</button>
+  
  </div>
+ <div>
+  <table class=" border border-solid">
+    <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Hobbies</th>
+      <th>Gender</th>
+    </tr>
+    <tr v-for="signUp in users" :key="signUp.id">
+    <td> {{signUp.id}}</td>
+    <td> {{signUp.name}}</td>
+   
+    </tr>
+  </table>
+ </div>
+ </div>
+
 </template>
 
 <script>
+    const users=[];
 export default {
+  
   name:"signUpForm",
+  signUp:{
+      id:null,
+      name:null,
+      hobbies:[],
+      gender:null
+      },
+ 
   data(){
     return {
       signUp:{
@@ -31,9 +59,14 @@ export default {
       gender:null
     }}
   },
+
   methods:{
+      
     submitFormValues(){
-      console.warn(this.signUp);
+      // console.warn(this.signUp);
+      users.push(this.signUp);
+      console.log(users);
+      
     }
   }
 }
