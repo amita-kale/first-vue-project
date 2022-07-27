bad7dd1a652e46c387d19f762088f9ede393d368
amita-kale <amitakale297@gmail.com>
2022-07-27 15:10:42 +0530

    taking different input fields

---
+++ src/components/signUpForm.vue
@@ -0,0 +1,24 @@
+<template>
+ 
+<div>
+  <h1 style="color:blue">signUp Form</h1>
+ 
+ ID:<input type="number"><br><br>
+ Name:<input type="text"><br><br>
+ Hobbies: &nbsp;&nbsp;<input type="checkbox" id="hob">
+ <label for="hob">Kho-Kho</label> &nbsp;
+ <input type="checkbox" id="hobi">
+ <label for="hobi">Cricket</label><br>
+<br>
+ Gender: &nbsp;&nbsp;<input type="radio" id="Female" name="gender">
+ <label for="Female">Female</label>&nbsp;
+ <input type="radio" id="male" name="gender">
+ <label for="male">Male</label>
+ </div>
+</template>
+
+<script>
+export default {
+  name:"signUpForm"
+}
+</script>
