68bd2d12bee39ebdb1fe7f8bca665dcf8ba6c2a3
amita-kale <amitakale297@gmail.com>
2022-07-27 10:39:27 +0530

    added style binding and class binding with object

---
+++ src/components/ClassStyleBinding/classstyle.vue
@@ -0,0 +1,27 @@
+<template>
+    <h1 class="changecolor">Class and Style Binding</h1><br>
+    <h2 v-bind:style="{color:'red'}">Style Binding</h2><br>
+    <h3 v-bind:class="{display:show,displayColor:show}">Class binding with object</h3>
+
+</template>>
+
+<script>
+
+
+export default({
+name:"classStyle",
+data(){
+    return {show:true};
+}
+})
+</script>
+
+
+<style scoped>
+.changecolor{
+    color: blue;
+}
+.display{
+    background-color: aqua;
+}
+</style>
