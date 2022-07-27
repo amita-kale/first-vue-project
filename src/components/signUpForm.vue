9ef8d4a9b9939cc4cdda4988fa3c9fe93afa642b
amita-kale <amitakale297@gmail.com>
2022-07-27 15:28:49 +0530

    added value attribute in gender feild

--- src/components/signUpForm.vue
+++ src/components/signUpForm.vue
@@ -3,22 +3,38 @@
 <div>
   <h1 style="color:blue">signUp Form</h1>
  
- ID:<input type="number"><br><br>
- Name:<input type="text"><br><br>
- Hobbies: &nbsp;&nbsp;<input type="checkbox" id="hob">
+ ID:<input type="number" v-model="signUp.id"><br><br>
+ Name:<input type="text" v-model="signUp.name"><br><br>
+ Hobbies: &nbsp;&nbsp;<input type="checkbox" id="hob" v-model="signUp.hobbies">
  <label for="hob">Kho-Kho</label> &nbsp;
- <input type="checkbox" id="hobi">
+ <input type="checkbox" id="hobi" v-model="signUp.hobbies">
  <label for="hobi">Cricket</label><br>
 <br>
- Gender: &nbsp;&nbsp;<input type="radio" id="Female" name="gender">
- <label for="Female">Female</label>&nbsp;
- <input type="radio" id="male" name="gender">
- <label for="male">Male</label>
+ Gender: &nbsp;&nbsp;<input type="radio" id="female" value="female" name="gender" v-model="signUp.gender">
+ <label for="female">Female</label>&nbsp;
+ <input type="radio" id="male" value="male" name="gender" v-model="signUp.gender">
+ <label for="male">Male</label><br>
+ <br>
+ <button type="submit" v-on:click="submitFormValues()">Submit</button>
  </div>
 </template>
 
 <script>
 export default {
-  name:"signUpForm"
+  name:"signUpForm",
+  data(){
+    return {
+      signUp:{
+      id:null,
+      name:null,
+      hobbies:[],
+      gender:null
+    }}
+  },
+  methods:{
+    submitFormValues(){
+      console.warn(this.signUp);
+    }
+  }
 }
 </script>
