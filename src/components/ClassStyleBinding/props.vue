99601448ed7a9dc2f807100e8cc94d2e24665732
amita-kale <amitakale297@gmail.com>
2022-07-27 11:53:08 +0530

    added props in child component

---
+++ src/components/ClassStyleBinding/props.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>{{name}}</h1>
+</template>
+<script>
+import { propsToAttrMap } from '@vue/shared';
+
+export default {
+    name:propsToAttrMap,
+    props:['name']
+}
+</script>
