31bac862c2ffc758b4102d8794795b0e0546ca3a
amita-kale <amitakale297@gmail.com>
2022-07-27 11:29:13 +0530

    did data binding using textarea and text printed in new line

--- src/components/ClassStyleBinding/classstyle.vue
+++ src/components/ClassStyleBinding/classstyle.vue
@@ -2,9 +2,9 @@
     <h1 class="changecolor">Data & Class and Style Binding</h1><br>
     <h2 v-bind:style="{color:'red'}">Style Binding</h2><br>
     <h3 v-bind:class="{display:'show',displayColor:'show'}">Class binding with object</h3>
-    <h1>{{msg}}</h1>
+    <h1 style="white-space:pre-line">{{msg}}</h1>
     <input type="text" v-model="msg"> <br>
-    
+
     <br>
     <textarea type="text" v-model="msg" />
 
